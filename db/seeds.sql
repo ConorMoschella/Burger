6d4bdb1c75794fdc9d85996343ac1c0496b67d57
Tetsukabe <Conmos13@gmail.com>
2018-11-11 11:27:13 -0500

    Final Copy

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('American');
+INSERT INTO burgers (name) VALUES ('Cheddar');
+INSERT INTO burgers (name, sleepy) VALUES ('Bacon Cheese', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Chicken', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Turkey', true);
+INSERT INTO burgers (name) VALUES ('Vegan');
